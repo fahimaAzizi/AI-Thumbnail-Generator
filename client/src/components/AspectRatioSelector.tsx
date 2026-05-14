9a6289a0f810a66566a7bd6375ee8dcb96dd977f
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-14 19:15:36 +0500

    update

--- client/src/components/AspectRatioSelector.tsx
+++ client/src/components/AspectRatioSelector.tsx
@@ -1,10 +1,15 @@
-import { RectangleHorizontal } from 'lucide-react'
-import React from 'react'
+import { RectangleHorizontal, RectangleVertical, Square } from 'lucide-react'
+import {aspectRatioSelector , type AspectRatio} from '../assets/assets'
+import type React from 'react'
+const AspectRatioSelector = ({}) => {
 
-const AspectRatioSelector = () => {
+
+
     const iconMap ={
-   '16:9' :<RectangleHorizontal className='siz-6'/>
-    }
+   '16:9' :<RectangleHorizontal className='siz-6'/>,
+   '1:1' : <Square className='siz-6'/>,
+   '9:16' :<RectangleVertical className='siz-6'/>
+    } as Recod<AspectRatio ,React.RractNode>
   return (
     <div className='space-y-3 dark'>
         
