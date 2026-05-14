896cb363edf5f8203d3de139fbd472e44b068d35
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-14 21:22:18 +0500

    update

--- client/src/components/AspectRatioSelector.tsx
+++ client/src/components/AspectRatioSelector.tsx
@@ -1,7 +1,7 @@
 import { RectangleHorizontal, RectangleVertical, Square } from 'lucide-react'
-import {aspectRatioSelector , type AspectRatio} from '../assets/assets'
+import {AspectRatio, aspectRatioSelector} from '../assets/assets'
 import type React from 'react'
-const AspectRatioSelector = ({}) => {
+const AspectRatioSelector = ({value, onChange} :{value: AspectRatio ; onchange: ( ratio : AspectRatio)=> void}) => {
 
 
 
@@ -9,12 +9,13 @@ const AspectRatioSelector = ({}) => {
    '16:9' :<RectangleHorizontal className='siz-6'/>,
    '1:1' : <Square className='siz-6'/>,
    '9:16' :<RectangleVertical className='siz-6'/>
-    } as Recod<AspectRatio ,React.RractNode>
+    } as Record<AspectRatio , React.ReactNode>
   return (
     <div className='space-y-3 dark'>
         
         <label className='block text-sn font-medium text-zinc-200'> Aspect Raio</label>
        <div className='flex flex-wrap gap-2'>
+        {AspectRatio.map}
 
        </div>
     </div>
