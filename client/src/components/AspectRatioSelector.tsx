bbac127a53baa5efe4502cf4c84e2b259dc781e7
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-15 23:18:06 +0500

    update

--- client/src/components/AspectRatioSelector.tsx
+++ client/src/components/AspectRatioSelector.tsx
@@ -1,33 +1,46 @@
 import { RectangleHorizontal, RectangleVertical, Square } from 'lucide-react'
-import {AspectRatio, aspectRatioSelector} from '../assets/assets'
+import { AspectRatio, aspectRatioSelector } from '../assets/assets'
 import type React from 'react'
-const AspectRatioSelector = ({value, onChange} :{value: AspectRatio ; onchange: ( ratio : AspectRatio)=> void}) => {
 
+const AspectRatioSelector = ({
+  value,
+  onChange,
+}: {
+  value: AspectRatio
+  onChange: (ratio: AspectRatio) => void
+}) => {
 
-
-    const iconMap ={
-   '16:9' :<RectangleHorizontal className='siz-6'/>,
-   '1:1' : <Square className='siz-6'/>,
-   '9:16' :<RectangleVertical className='siz-6'/>
-    } as Record<AspectRatio , React.ReactNode>
-
+  const iconMap = {
+    '16:9': <RectangleHorizontal className='size-6' />,
+    '1:1': <Square className='size-6' />,
+    '9:16': <RectangleVertical className='size-6' />
+  } as Record<AspectRatio, React.ReactNode>
 
   return (
     <div className='space-y-3 dark'>
-        
-        <label className='block text-sn font-medium text-zinc-200'> Aspect Raio</label>
-       <div className='flex flex-wrap gap-2'>
-        {AspectRatio.map((ratio)=>{
-          const selected = value === ratio;
+
+      <label className='block text-sm font-medium text-zinc-200'>
+        Aspect Ratio
+      </label>
+
+      <div className='flex flex-wrap gap-2'>
+        {aspectRatioSelector.map((ratio) => {
+          const selected = value === ratio
 
           return (
-            <button key={ratio}>
-              {iconMap[rotio]}
+            <button
+              key={ratio}
+              type='button'
+              onClick={() => onChange(ratio)}
+              className={`flex items-center gap-2 rounded-md border px-5 py-2.5 text-sm transition border-white/10
+              ${selected ? 'bg-white/10' : 'hover:bg-white/6'}`}
+            >
+              {iconMap[ratio]}
+              <span className='tracking-widest'>{ratio}</span>
             </button>
           )
         })}
-
-       </div>
+      </div>
     </div>
   )
 }
