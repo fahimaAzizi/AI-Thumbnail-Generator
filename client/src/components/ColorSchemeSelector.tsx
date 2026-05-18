d1cd221b30fdb561225b5e346a74f0f1100a6b01
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-18 16:16:02 +0500

    update

---
+++ client/src/components/ColorSchemeSelector.tsx
@@ -0,0 +1,12 @@
+import { colorSchemes } from "../assets/assets"
+
+const ColorSchemeSelector = ({value, onChange}: {value :string;
+    onChange: (coloe: string)=>void })  => {
+ 
+  return (
+    <div className="space-y-3">
+    </div>
+  )
+}
+
+export default ColorSchemeSelector
