ec5850a81adf7f6cd0d9f96a6262f154bd2653d8
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-20 17:18:45 +0500

    update

--- client/src/components/PreviewPanel.tsx
+++ client/src/components/PreviewPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { AspectRatio } from '../assets/assets';
 import { DownloadIcon, Loader2Icon } from 'lucide-react';
+import { div } from 'motion/react-m';
 
 const PreviewPanel = ({thumbnail, isLoading, aspectRatio} : 
   {thumbnail: IThumbnail, isLoading: boolean; aspectRatio: AspectRatio}) =>{
@@ -12,6 +13,12 @@ const PreviewPanel = ({thumbnail, isLoading, aspectRatio} :
   '1:1': 'aspect-square',
   '9:16': 'aspect-[9/16]',
 } as Record<AspectRatio, string>;
+
+
+const onDownload =() =>{
+  if (!thumbnail.image_url) return;
+  window.open(thumbnail.image_url, '_blank')
+}
 
   return (
    <div className="relative mx-auto w-full max-w-2xl">
@@ -31,8 +38,8 @@ const PreviewPanel = ({thumbnail, isLoading, aspectRatio} :
       className="h-full w-full object-cover"
     />
 
-    <div>
-      <button type="button">
+    <div className='absolute instt-0 flex items-end justify-center bg-black/10 opacity-0 tarnsition-opacity group-hover:opacity-100'>
+      <button onClick={onDownload} type="button" className='md-6 flex items-center gap-2 rounded-md px-5 py-2.5 text-xs font-medium transition bg-white/30 ring-2 ring-white/40 backdrop-blur hover:scale-105 avtive:scale-95'>
         <DownloadIcon className="size-4" />
         Download Thumbnail
       </button>
@@ -40,6 +47,10 @@ const PreviewPanel = ({thumbnail, isLoading, aspectRatio} :
   </div>
 )}
        </div>
+      )}
+
+      {!isLoading && !thumbnail?.image_url && (
+        <div></div>
       )}
     </div>
   </div>
