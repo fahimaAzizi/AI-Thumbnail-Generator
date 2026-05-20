f1d73c090a61d283c0209783700638f84a01269e
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-20 17:26:51 +0500

    update

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -6,6 +6,7 @@ import AspectRatioSelector from '../components/AspectRatioSelector'
 import { colorSchemes } from '../assets/assets'
 import StyleSelector from '../components/StyleSelector'
 import ColorSchemeSelector from '../components/ColorSchemeSelector'
+import PreviewPanel from '../components/PreviewPanel'
 
 const Generate = () => {
   const { id } = useParams()
@@ -119,6 +120,14 @@ const Generate = () => {
 
               </div>
 
+            </div>
+             <div>
+            <div className='p-6 rounded-2xl bg-white/8 border border-white/10 shadow-xl'>
+            <h2 className='text-lg font-semibold text-zinc-100'> Perview</h2>
+              <PreviewPanel thumbnail={thumbnail} isLoading={loading} aspectRatio={aspectRatio}/>
+             
+
+              </div>
             </div>
 
           </div>
