e9aa06dfa027f41c957277e8362e913dbd967fa9
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-22 22:39:05 +0500

    update

--- client/src/pages/MyGeneration.tsx
+++ client/src/pages/MyGeneration.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import SoftBackdrop from '../components/SoftBackdrop'
 
 function MyGeneration() {
   return (
-    <div>MyGeneration</div>
+    <>
+
+    <SoftBackdrop/>
+    <div className='mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:px-32'>
+
+      {/* HEADER */}
+      <div>
+        
+      </div>
+
+    </div>
+
+    </>
   )
 }
 
