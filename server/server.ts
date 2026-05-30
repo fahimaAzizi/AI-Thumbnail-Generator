ce04502d972b12684d103adb592d364bf1934fb9
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-30 23:25:37 +0500

    update

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,23 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors'
 
 
 
 const app = express();
 
-const port =  3000;
+app.use(cors())
+app.use(express.json())
+
+
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
