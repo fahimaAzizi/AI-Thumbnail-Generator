4f3117a513389edc0227815fdb225516ff2eb64d
Fahima Azizi <fahimaazizi397@gmail.com>
2026-06-02 18:14:55 +0500

    update

--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,28 @@ import express, { Request, Response } from 'express';
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js';
+import session from 'express-session';
 
-
+ declare module 'express-session' {
+interface SessionData{
+    isLoggedIn : boolean;
+    userId: string
+}
+ }
 
 await connectDB();
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:['http://lcalhost:5173','http://lcalhost:3000'],
+    credentials: true 
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave : false,
+}))
 app.use(express.json())
 
 
