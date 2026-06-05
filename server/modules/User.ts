41276996eabac8642f0f997a1e9d855bad773a0f
Fahima Azizi <fahimaazizi397@gmail.com>
2026-06-05 23:45:49 +0500

    update

--- server/modules/User.ts
+++ server/modules/User.ts
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema<Iuser>({
     name: {type: String , required : true, trim: true},
     email: {type: String , required : true, trim: true, unique : true,
         lowercase : true},
+        password: {type: String, required:true}
     
  
  
