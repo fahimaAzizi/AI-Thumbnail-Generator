9e5c5f415b3387b729831aa1de3e39bf23dbbbad
Fahima Azizi <fahimaazizi397@gmail.com>
2026-06-10 14:13:58 +0500

    update

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -12,7 +12,27 @@ export const getUsersThumbnails = async (req: Request, res: Response) => {
     
     const thumbnails = await Thumbnail.find({ userId }).sort({ createdAt: -1 });
     
-    res.json({ thumbnails });
+ // Controllers to get single Thumbnail of a User
+export const getThumbnailById = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.session;
+    const { id } = req.params;
+
+    if (!userId) {
+      return res.status(401).json({ message: 'User not authenticated' });
+    }
+
+    if (!id) {
+      return res.status(400).json({ message: 'Thumbnail ID is required' });
+    }
+
+    const thumbnail = await Thumbnail.findOne({ userId, _id: id });
+
+    if (!thumbnail) {
+      return res.status(404).json({ message: 'Thumbnail not found' });
+    }
+
+    res.json({ thumbnail });
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ message: error.message });
