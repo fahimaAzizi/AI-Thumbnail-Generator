0ea36dcd28e533ec5705a9c57d2607b7ff1a8c86
Fahima Azizi <fahimaazizi397@gmail.com>
2026-06-11 23:15:14 +0500

    update

---
+++ client/src/context/AuthOntext.tsx
@@ -0,0 +1,26 @@
+ import { createContext, ReactNode, useState } from "react";
+import type { IUser } from "../types/user"; // adjust path
+
+interface AuthContextProps {
+  isLoggedIn: boolean;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
+  user: IUser | null;
+  setUser: (user: IUser | null) => void;
+  login: (user: IUser) => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextProps | undefined>(
+  undefined
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const login = async (user: IUser) => {
+    setUser(user);
+    setIsLoggedIn(true);
+  };
+
+
+};
