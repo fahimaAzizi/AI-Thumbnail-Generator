04a0b59ff990c21b8c106bbb2861bc74070b9e62
Fahima Azizi <fahimaazizi397@gmail.com>
2026-06-15 23:37:13 +0500

    update

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -9,7 +9,7 @@ interface AuthContextProps {
   setUser: (user: IUser | null) => void;
   login: (user: {email: string; password: string}) => Promise<void>;
   signUp: (user: {name:string; email: string; password: string}) => Promise<void>;
-   logout: () => Promise<void>
+  logout: () => Promise<void>
   }
 
 const AuthContext = createContext<AuthContextProps>({
@@ -42,10 +42,48 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       console.log(error)      
     }
   }
-  const login = async (user: IUser) => {
-    setUser(user);
-    setIsLoggedIn(true);
+
+  
+   
+
+  const login = async ({ email, password} : { email: string ; password: string}) =>{
+    try {
+      const {data} =await api.post('/api/auth/login',{email,password});
+      if(data.user){
+        setUser(data.user as IUser)
+        setIsLoggedIn(true)
+      }
+      toast.success(data(data.message))
+      
+    } catch (error) {
+      console.log(error)      
+    }
+  
   };
+  const logout = async ()=>{
+     try {
+      const {data} =await api.post('/api/auth/logout');
+      setUser(null)
+        setIsLoggedIn(false)
+      toast.success(data(data.message))
+      
+      
+    } catch (error) {
+      console.log(error)      
+    }
+  }
+  const fetchUser = async ()=>{
+       try {
+      const {data} =await api.post('/api/auth/logout');
+      setUser(null)
+        setIsLoggedIn(false)
+      toast.success(data(data.message))
+      
+      
+    } catch (error) {
+      console.log(error)      
+    }
+  }
 
   return (
     <AuthContext.Provider
