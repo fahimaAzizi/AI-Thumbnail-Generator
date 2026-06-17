422b69e54771433281192aaca5b0731078ae5b4c
Fahima Azizi <fahimaazizi397@gmail.com>
2026-06-17 17:30:19 +0500

    update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { motion } from "motion/react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { button } from "motion/react-client";
 
 export default function Navbar() {
     const {isLoggedIn, user,logout} = useAuth
@@ -32,13 +33,28 @@ export default function Navbar() {
                     
                 
                 </div>
-                <div>
-                    
-                </div>
-
-                <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                <div className="flex items-center gap-2">
+                    {isLoggedIn ? (
+                        <div className="relative group">
+                            <button>
+                                {user?.name.charAt(0).toUpperCase()}
+                            </button>
+                            <div className="absolute hidden group-hover:block top-6 right-0 pt-4">
+                                <button onClick={()=>logout()} className="bg-white/20 border-2 border-white/10 px-5 py-1.5 rounded">
+                                    Logout
+                                </button>
+                                
+                            </div>
+                        </div>
+                    ) : (
+                        <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
                     Start free trial
                 </button>
+                    )}
+
+                </div>
+
+                
                 <button onClick={() => setIsOpen(true)} className="md:hidden">
                     <MenuIcon size={26} className="active:scale-90 transition" />
                 </button>
@@ -47,9 +63,17 @@ export default function Navbar() {
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                <Link onClick={() => setIsOpen(false)} to='/'>Home</Link>
                <Link onClick={() => setIsOpen(false)} to='generate'>Generate</Link>
+               {isLoggedIn ? <Link onClick={()=> setIsOpen(false)} to='/my-generation'>Generate</Link>
+               :
+               <Link onClick={()=> setIsOpen(false)} to='#'>About</Link>  }
                <Link onClick={() => setIsOpen(false)} to='/my-generation'>My Generations</Link>
-               <Link onClick={() => setIsOpen(false)} to='#'>My Contact</Link>
-               <Link onClick={() => setIsOpen(false)} to='/login'>Login</Link>
+               <Link onClick={() => setIsOpen(false)} to='#'>Contact us</Link>
+               { isLoggedIn ?
+               <button onClick={()=>{setIsOpen(false); logout()}}>Logout</button>
+               :
+                <Link onClick={() => setIsOpen(false)} to='/login'>Login</Link>
+               }
+               
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
                     <XIcon />
                 </button>
